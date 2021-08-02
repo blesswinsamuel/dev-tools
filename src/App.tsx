f8bea8ccd7cfc38885ed269c7aa90f78acb5c387
Blesswin Samuel <blesswinsamuel@gmail.com>
2021-08-02 12:51:56 +0530

    add goatcounter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Router, Switch, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Base64 from './pages/Base64'
 import CaseConverter from './pages/CaseConverter'
@@ -7,12 +7,21 @@ import Home from './pages/Home'
 import Timestamp from './pages/Timestamp'
 import UriComponentEndecode from './pages/UriComponentEndecode'
 import UriEndecode from './pages/UriEndecode'
+import { createBrowserHistory } from 'history'
+
+var history = createBrowserHistory()
+
+history.listen((location) => {
+  ;(window as any).goatcounter.count({
+    path: location.pathname + location.search,
+  })
+})
 
 const Avro = React.lazy(() => import('./pages/Avro'))
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Layout>
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
